1a4859a55679b1783b00fd15393958f988eb6681
walluri <prakash4455@gmail.com>
2019-09-21 14:06:00 +0530

    wcmel - created react app

---
+++ wcmel/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = ()=>{
+    return (
+        <div>We Care Melourne</div>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
