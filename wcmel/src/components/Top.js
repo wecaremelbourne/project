17db79894c56c69025049456b636041c7d56d135
satya-valluri <prakash4455@gmail.com>
2019-09-24 17:25:56 +0530

    SPA Frame created to work on

--- wcmel/src/components/Top.js
+++ wcmel/src/components/Top.js
@@ -2,19 +2,24 @@ import React from 'react'
 import '../css/style.css'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Home from './home'
+import Matter1 from './/Matter1'
+import Matter2 from './Matter2'
 
 
-const Topbar = () => {
+
+const MainApp = () => {
     return (
         <section class='topbar'>
             <div >
                 <Router>
                     <ul>
-                        <li>
-                            <Link to="/">Home Link</Link>
-                        </li>
+                        <li>  <Link to="/">Home</Link>  </li>
+                        <li>  <Link to="/matter1">matter1</Link>  </li>
+                        <li>  <Link to="/matter2">matter2</Link>  </li>
                     </ul>
                     <Route exact path="/" component={Home} />
+                    <Route exact path="/matter1" component={Matter1} />
+                    <Route exact path="/matter2" component={Matter2} />
                 </Router>
             </div>
 
@@ -22,4 +27,4 @@ const Topbar = () => {
     )
 }
 
-export default Topbar;
+export default MainApp;
--- wcmel/src/index.js
+++ wcmel/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Topbar from './components/Top'
-import Middlebar from './components/middle';
-import Bottombar from './components/bottom';
+import MainApp from './components/Top'
 
 
 const App = () => {
     return (
         <div>
-            <Topbar/>
+            <MainApp/>
         </div>
     ) 
 }
